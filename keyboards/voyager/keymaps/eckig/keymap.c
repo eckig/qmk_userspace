77d75a7b09796aa00fcf6e10bb6a16dd00697833
eckig <eckig@users.noreply.github.com>
2025-09-04 12:59:37 +0200

    .

--- keyboards/voyager/keymaps/eckig/keymap.c
+++ keyboards/voyager/keymaps/eckig/keymap.c
@@ -121,3 +121,23 @@ void matrix_scan_user(void) {
   }
 }
 
+bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
+                      uint16_t other_keycode, keyrecord_t* other_record) {
+    // allow ctrl+shift directly:
+    switch (tap_hold_keycode) {
+        case MT_CTL_ESC:
+            if (other_keycode == KC_LSFT) {
+                return true;
+            }
+            break;
+
+        case MT_CTL_MIN:
+            if (other_keycode == KC_RSFT) {
+                return true;
+            }
+            break;
+    }
+    // Otherwise defer to default:
+    return get_chordal_hold_default(tap_hold_record, other_record);
+}
+
